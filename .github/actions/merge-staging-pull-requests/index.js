b7d197d2bb1b4393f0a88986904ef63d5bf399f4
Aymen Rebouh <aymenmse@gmail.com>
2022-06-13 08:42:17 +0900

    Fix

--- .github/actions/merge-staging-pull-requests/index.js
+++ .github/actions/merge-staging-pull-requests/index.js
@@ -14,7 +14,7 @@ async function run() {
     console.log(`owner=${owner}, repo=${repo}`);
     const data = await fetchStagingPRs();
     data.forEach(pr => {
-        console.log(`PR ${pr.title} url=${pr.url}, isDraft=${pr.draft == null || pr.draft == false}, prLabels=${pr.labels}, hasStaging=${pr.labels.some(l => l.name != nil && l.name === "Staging")}`)
+        console.log(`PR ${pr.title} url=${pr.url}, isDraft=${pr.draft == null || pr.draft == false}, prLabels=${pr.labels}, hasStaging=${pr.labels.some(l => l.name != undefined && l.name === "Staging")}`)
     });
     console.log("data=");
     console.log(data)
