f98f631711715f4182028c743dc5dbbf52619644
Aymen Rebouh <aymenmse@gmail.com>
2022-06-13 08:03:35 +0900

    changes

--- .github/actions/merge-staging-pull-requests/index.js
+++ .github/actions/merge-staging-pull-requests/index.js
@@ -5,13 +5,19 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 try {
-const { data } = octokit.rest.pulls.list({
-    owner,
-    repo,
-    state: "open",
-  });
+const data = await fetchPRs();
   console.log(`PRs data ${data}!`);
+  console.log(data);
   
 } catch (error) {
   core.setFailed(error.message);
 }
+
+async function fetchPRs() {
+    const data = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        state: "open",
+      });
+      return data
+  }
